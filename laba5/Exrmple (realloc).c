519520a8c74b8e4153cbb47503798fc7f207efc2
Oleg Podnebesov <sovmesti@gmail.com>
2024-06-15 11:02:34 +0300

    Initial commit

---
+++ laba5/Exrmple (realloc).c
@@ -0,0 +1,36 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+
+	int* x = NULL;
+	int number, i, found;
+	int count = 0;
+	while (printf("Есть что записать? "), scanf_s("%i", &number)) {
+		found = 0;
+		for (i = 0; i < count; i++) {
+			if (number == x[i]) {
+				found = 1;
+				break;
+			}
+		}
+		if (!found) {
+			x = realloc(x, ++count * sizeof(int));
+			if (x == NULL) {
+				printf("Ошибка выделения памяти");
+				return 1;
+			}
+			x[count - 1] = number;
+		}
+	}
+	printf("Вы ввели: ");
+	for (i = 0; i < count; i++) 
+		printf("%3i", x[i]);
+	
+	free(x);
+	x = NULL;
+	return 0;
+}
